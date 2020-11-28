f6fce7539e33d4d5bb1a691c4a5e690ad5befb0b
xf8b <unknownperson23fs6zc@protonmail.com>
2020-11-27 16:38:21 -0800

    license headers and some other misc stuff

---
+++ .github/ISSUE_TEMPLATE/other.md
@@ -0,0 +1,9 @@
+---
+name: Other
+about: Create a custom issue (not recommended, please use the others)
+title: ''
+labels: type:other
+assignees: ''
+
+---
+
--- src/main/kotlin/io/github/blueminecraftteam/healthmod/registries/StatusEffectRegistries.kt
+++ src/main/kotlin/io/github/blueminecraftteam/healthmod/registries/StatusEffectRegistries.kt
@@ -16,6 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with HealthMod.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package io.github.blueminecraftteam.healthmod.registries
 
 import io.github.blueminecraftteam.healthmod.statuseffects.WoundInfectionStatusEffect
