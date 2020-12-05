a4748174a16cf0b4f2bf51a07de143d2dff448e0
xf8b <unknownperson23fs6zc@protonmail.com>
2020-12-05 09:03:08 -0800

    license header

--- src/main/kotlin/io/github/blueminecraftteam/healthmod/items/BloodVialItem.kt
+++ src/main/kotlin/io/github/blueminecraftteam/healthmod/items/BloodVialItem.kt
@@ -1,6 +1,26 @@
+/*
+ * Copyright (c) 2020 Blue Minecraft Team.
+ *
+ * This file is part of HealthMod.
+ *
+ * HealthMod is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HealthMod is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HealthMod.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package io.github.blueminecraftteam.healthmod.items
 
 import net.minecraft.item.Item
 
-class BloodVialItem(settings: Settings) : Item(settings)  {
+class BloodVialItem(settings: Settings) : Item(settings) {
+    // TODO create blood machine to use this on
 }
