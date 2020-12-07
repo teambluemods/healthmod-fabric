444d2ca0be786172da1c3a1557cdf54b6f22eefd
secnyt <47509759+secnyt@users.noreply.github.com>
2020-12-06 19:18:41 -0600

    allow to be compiled
    license and stupid strict override

--- src/main/kotlin/io/github/blueminecraftteam/healthmod/items/IsopropylAlcoholItem.kt
+++ src/main/kotlin/io/github/blueminecraftteam/healthmod/items/IsopropylAlcoholItem.kt
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2020 Blue Minecraft Team.
+ *
+ * This file is part of HealthMod.
+ *
+ * HealthMod is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HealthMod is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HealthMod.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package io.github.blueminecraftteam.healthmod.items
 
 import net.minecraft.entity.player.PlayerEntity
@@ -7,9 +26,9 @@ import net.minecraft.util.TypedActionResult
 import net.minecraft.world.World
 
 class IsopropylAlcoholItem {
-    override fun use(world: World, user: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> {
+    /*override fun use(world: World, user: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> {
         if(!world.isClient) {
 
         }
-    }
+    }*/
 }
