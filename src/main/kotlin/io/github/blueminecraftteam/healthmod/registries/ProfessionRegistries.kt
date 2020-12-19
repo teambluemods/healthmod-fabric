014165be2882c777051215e0e91342df0ee51daf
xf8b <unknownperson23fs6zc@protonmail.com>
2020-12-18 16:48:13 -0800

    fix license headers

--- src/main/kotlin/io/github/blueminecraftteam/healthmod/registries/ProfessionRegistries.kt
+++ src/main/kotlin/io/github/blueminecraftteam/healthmod/registries/ProfessionRegistries.kt
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2020 Blue Minecraft Team.
+ *
+ * This file is part of HealthMod.
+ *
+ * HealthMod is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HealthMod is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with HealthMod.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package io.github.blueminecraftteam.healthmod.registries
 
 import io.github.blueminecraftteam.healthmod.HealthMod
