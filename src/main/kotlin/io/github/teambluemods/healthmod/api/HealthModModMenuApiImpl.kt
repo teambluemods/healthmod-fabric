1c94b2023e8fdd52ecdb3e4620d844a75d925c7a
xf8b <unknownperson23fs6zc@protonmail.com>
2021-02-09 14:35:17 -0800

    switch to non deprecated classes

--- src/main/kotlin/io/github/teambluemods/healthmod/api/HealthModModMenuApiImpl.kt
+++ src/main/kotlin/io/github/teambluemods/healthmod/api/HealthModModMenuApiImpl.kt
@@ -19,8 +19,8 @@
 
 package io.github.teambluemods.healthmod.api
 
-import io.github.prospector.modmenu.api.ConfigScreenFactory
-import io.github.prospector.modmenu.api.ModMenuApi
+import com.terraformersmc.modmenu.api.ConfigScreenFactory
+import com.terraformersmc.modmenu.api.ModMenuApi
 import io.github.teambluemods.healthmod.config.HealthModConfig
 import me.sargunvohra.mcmods.autoconfig1u.AutoConfig
 import net.fabricmc.api.EnvType
